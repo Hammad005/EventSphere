ce77eb947aa13b5a6957f54cd2f99766a0ef1d94
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-11 22:04:31 +0500

    ✨ feat(dashboard): enhance dashboard ui and navigation
    - improve navbar responsiveness with dynamic text size
    - add sidebar navigation with icons and active state highlighting
    - include links for dashboard, manage events, users, and profile
    - enhance logo animation on sidebar

--- client/src/components/DashboardNavbar.jsx
+++ client/src/components/DashboardNavbar.jsx
@@ -10,7 +10,7 @@ const DashboardNavbar = () => {
     <>
       <nav className="w-full p-3 flex items-center justify-between sticky top-0 z-50 dark:bg-black/90 bg-secondary border-b dark:border-black border-zinc-300">
         <div className="flex items-center justify-between w-full">
-          <h1 className="text-3xl font-serif tracking-wider font-bold first-letter:uppercase">
+          <h1 className="md:text-3xl text-2xl font-serif tracking-wider font-bold first-letter:uppercase">
             {location.pathname.split("/")}
           </h1>
           <div className="flex gap-2">
--- client/src/pages/sub-components/DashboardSidebar.jsx
+++ client/src/pages/sub-components/DashboardSidebar.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import { Loader2, LogOut } from "lucide-react";
+import { CalendarRange, Gauge, Loader2, LogOut, UserRound, UsersRound } from "lucide-react";
 import { useAuthStore } from "@/store/useAuthStore";
 import { Button } from "@/components/ui/button";
 
 const DashboardSidebar = () => {
   const { userLoading, signout } = useAuthStore();
+  const location = useLocation();
   return (
-    <aside className="flex flex-col justify-between dark:bg-secondary bg-zinc-100 border-r dark:border-black border-zinc-300 min-h-screen sticky top-0 w-full md:p-4">
-      <div className="w-full lg:p-0 p-2">
+    <aside className="flex flex-col justify-between md:gap-8 dark:bg-secondary bg-zinc-100 border-r dark:border-black border-zinc-300 h-screen sticky top-0 w-full md:p-4">
+      <div className="w-full lg:p-0 p-2 ">
         <Link to={"/"} className="flex flex-col items-center gap-2 w-full">
-          <div className="p-2 rounded-md bg-black w-12 h-12 object-contain flex justify-center">
+          <div className="p-2 rounded-md bg-black w-12 h-12 object-contain flex justify-center animate-bounce">
             <img
               src={logo}
               alt=" logo"
@@ -30,18 +31,65 @@ const DashboardSidebar = () => {
         </Link>
       </div>
 
-<div className={"md:p-0 p-2"}>
-      <Button onClick={signout} disabled={userLoading} className={'w-full'}>
-        {!userLoading ? (
-          <>
-          <span className="md:inline hidden">Log Out</span>
-            <LogOut />
-          </>
-        ) : (
-          <Loader2 className="animate-spin" />
-        )}
-      </Button>
-</div>
+      <div
+        className={
+          "md:p-0 p-2 flex flex-col gap-6 w-full h-full"
+        }
+      >
+        <Button
+          asChild
+          className={"w-full md:justify-between"}
+          variant={location.pathname === "/dashboard" ? "default" : "outline"}
+        >
+          <Link to={"/dashboard"}>
+            <Gauge />
+            <span className="md:inline hidden">Dashboard</span>
+          </Link>
+        </Button>
+        <Button
+          asChild
+          className={"w-full md:justify-between"}
+          variant={location.pathname === "/manage-Events" ? "default" : "outline"}
+        >
+          <Link to={"/manage-Events"}>
+            <CalendarRange />
+            <span className="md:inline hidden">Manage Events</span>
+          </Link>
+        </Button>
+        <Button
+          asChild
+          className={"w-full md:justify-between"}
+          variant={location.pathname === "/manage-Users" ? "default" : "outline"}
+        >
+          <Link to={"/manage-Users"}>
+            <UsersRound />
+            <span className="md:inline hidden">Manage Users</span>
+          </Link>
+        </Button>
+        <Button
+          asChild
+          className={"w-full md:justify-between"}
+          variant={location.pathname === "/profile" ? "default" : "outline"}
+        >
+          <Link to={"/profile"}>
+            <UserRound />
+            <span className="md:inline hidden">My Profile</span>
+          </Link>
+        </Button>
+      </div>
+
+      <div className={"md:p-0 p-2 h-full flex items-end"}>
+        <Button onClick={signout} disabled={userLoading} className={"w-full"}>
+          {!userLoading ? (
+            <>
+              <span className="md:inline hidden">Log Out</span>
+              <LogOut />
+            </>
+          ) : (
+            <Loader2 className="animate-spin" />
+          )}
+        </Button>
+      </div>
     </aside>
   );
 };
