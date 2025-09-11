7e80fca3f56b8ff35c8edf328071aac97bc10e7b
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-11 14:20:33 +0500

    ✨ feat(components): add card and label components with signup page
    - 【feat(components/ui)】: create reusable card component for consistent UI
    - add card, cardheader, cardtitle, carddescription, cardaction, cardcontent, cardfooter subcomponents
    - implement radix ui label component for accessibility
    - 【feat(pages)】: implement signup page layout
    - integrate card component to structure signup form
    - add input fields for user registration (name, email, phone, etc.)
    - implement basic form handling with state for input fields
    - 【build(dependencies)】: add radix-ui react-label and react-primitive dependencies
    - install @radix-ui/react-label for accessible label components
    - install @radix-ui/react-primitive as a dependency for @radix-ui/react-label

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { UserRoundPlus } from 'lucide-react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        department: "",
+        role: "participant",
+        password: "",
+    });
+    const [confirmPass, setConfirmPass] = useState("");
+    const [error, setError] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newError = {};
+
+        if (!data.name) {
+            newError.name = "Name is required";
+        }
+
+        if (!data.email) {
+            newError.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+            newError.email = "Invalid email format";
+        }
+
+        if (!data.phone) {
+            newError.phone = "Phone number is required";
+        }
+
+        if (!data.department) {
+            newError.department = "Department is required";
+        }
+
+        if (!data.password) {
+            newError.password = "Password is required";
+        } else if (data.password.length < 6) {
+            newError.password = "Password must be at least 6 characters long";
+        } else if (data.password !== confirmPass) {
+            newError.password = "Passwords do not match";
+        }
+    }
   return (
-    <div>
-        
+    <div className='flex items-center justify-center min-h-[calc(100vh-75px)] w-full md:px-0 px-6'>
+        <Card className={"md:w-1/2 w-full"}>
+            <CardHeader>
+                <CardTitle className={"flex items-center gap-2"}>
+                    <UserRoundPlus className='size-5'/>
+                    <span>
+                    SignUp - <span className='text-muted-foreground text-xs'>(Only for participant)</span>
+                    </span>
+                </CardTitle>
+                <CardDescription>
+                    SignUp and unleash your potential
+                </CardDescription>
+                <CardContent className={'flex flex-col gap-4 px-0 mt-2'}>
+                    <form onSubmit={handleSubmit}>
+                    <div className='flex flex-col gap-1'>
+                    <Label htmlFor="name">Full Name</Label>
+                    <Input type={"text"} placeholder='Enter your full name' value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
+                    </div>
+                    </form>
+                </CardContent>
+            </CardHeader>
+        </Card>
     </div>
   )
 }
